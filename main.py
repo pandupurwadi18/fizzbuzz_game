19121b86fc09a54ef3aa446dc6786f4c66e5aa73
unknown <pandupurwadi18@gmail.com>
2022-07-04 21:01:19 +0700

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+# --- Fizzbuzz Game ---
+# 100 days of code Python
+# Pandu Purwadi 
+
+for number in range (1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
